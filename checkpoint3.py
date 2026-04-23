e935f3f53b699e9a861a153b81f9686bf59a4740
olatzpv <pvolatz@gmail.com>
2026-04-23 09:19:03 +0200

    checkpoint 3

---
+++ checkpoint3.py
@@ -0,0 +1,49 @@
+# Exercise 1
+my_string = "Hello world"
+my_number = 10
+my_list = [1, 2, 3, 4]
+my_boolean = True
+
+# Exercise 2
+first_three_letters = my_string[0:3]
+print(first_three_letters)
+
+# Exercise 3
+first_element = my_list[0]
+print(first_element)
+
+# Exercise 4
+new_number = my_number + 10
+print(new_number)
+
+# Exercise 5
+last_element = my_list[-1]
+print(last_element)
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+print(names_list)
+
+# Exercise 7
+first_word = my_string[0:5].upper()
+new_string = first_word + my_string[5:]
+print(new_string)
+
+# Exercise 8
+print(f"Mi número es {my_number}")
+
+# Exercise 9
+print("hello world")
+#print(my_string)  es otra opcion
+
+#EXTRA
+text = "Hola mundo, esto es un ejemplo"
+
+# Buscar "Hola"
+found = text[0:4]
+print(found)
+
+# Reemplazar "Hola" por "adiós"
+new_text = text.replace("Hola", "Adiós")
+print(new_text)
